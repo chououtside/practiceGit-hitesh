cacc8cc97810886a24c2f43e6aa6f437f16e85f2
chououtside <chouming3@gmail.com>
2016-01-10 09:05:01 -0800

    added a new file

---
+++ alex.js
@@ -0,0 +1,20 @@
+var checkChar = function(char, string){
+  //if the string equals '' return zero
+  if (string === "") {
+    return 0;
+  }
+  
+  //if character equals first letter in the string return 1 plus function('char', 
+  //substring(starting at index1)
+  if (char === string[0]){
+    return 1 + checkChar(char, string.substring(1));
+  }
+  
+  //if not equal return 0 plus plus function('char', 
+  //substring(starting at index1)
+  else{
+    return 0 + checkChar(char, string.substring(1));
+  }
+  
+
+};
